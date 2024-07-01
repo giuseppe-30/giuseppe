b91a8130a6b3d64ffc10376886381c5c2b22d0e2
giuseppe-30 <161639213+giuseppe-30@users.noreply.github.com>
2024-07-01 18:33:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+
+const viaggi = [
+    {
+        destinazione: "Parigi, Francia",
+        durata: "5 giorni",
+        costo: "1200€",
+        attivita: ["Tour della Torre Eiffel", "Visita al Louvre", "Crociera sulla Senna"]
+    },
+    {
+        destinazione: "Tokyo, Giappone",
+        durata: "7 giorni",
+        costo: "2500€",
+        attivita: ["Visita al tempio Senso-ji", "Tour di Akihabara", "Escursione sul Monte Fuji"]
+    },
+    {
+        destinazione: "New York, USA",
+        durata: "4 giorni",
+        costo: "1800€",
+        attivita: ["Statua della Libertà", "Central Park", "Museo di Arte Moderna"]
+    },
+    {
+        destinazione: "Roma, Italia",
+        durata: "3 giorni",
+        costo: "800€",
+        attivita: ["Colosseo", "Vaticano", "Fontana di Trevi"]
+    },
+    {
+        destinazione: "Sydney, Australia",
+        durata: "10 giorni",
+        costo: "3000€",
+        attivita: ["Opera House", "Bondi Beach", "Taronga Zoo"]
+    },
+    {
+        destinazione: "Londra, Regno Unito",
+        durata: "6 giorni",
+        costo: "1500€",
+        attivita: ["London Eye", "Torre di Londra", "Museo di Storia Naturale"]
+    },
+    {
+        destinazione: "Barcellona, Spagna",
+        durata: "5 giorni",
+        costo: "1100€",
+        attivita: ["Sagrada Familia", "Parc Güell", "Spiaggia di Barceloneta"]
+    },
+    {
+        destinazione: "Bangkok, Thailandia",
+        durata: "8 giorni",
+        costo: "2000€",
+        attivita: ["Gran Palazzo Reale", "Mercato Galleggiante", "Tempio del Buddha Sdraiato"]
+    },
+    {
+        destinazione: "Città del Capo, Sudafrica",
+        durata: "7 giorni",
+        costo: "2200€",
+        attivita: ["Table Mountain", "Robben Island", "Tour dei vigneti"]
+    },
+    {
+        destinazione: "Reykjavik, Islanda",
+        durata: "5 giorni",
+        costo: "1800€",
+        attivita: ["Laguna Blu", "Tour dei Geyser", "Cascata Gullfoss"]
+    }
+];
+ 
+
+ const numeroTotaleViaggi = viaggi.length;
+window.alert( " Ecco quanti viaggi ci sono:"   +  numeroTotaleViaggi )
